f9b7c25d64b46f41857ab2c3fa87ce25acae5c05
Oleh Safronov <osafronov@lenovo.com>
2023-11-30 12:24:58 +0200

    Working with Atlantis + terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1" # Set your desired AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "demo-atlantis-bucket" # Set a unique bucket name
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-atlantis-state" # Set a unique bucket name for storing Terraform state
+    key     = "terraform.tfstate"
+    region  = "us-east-1" # Set the region of the state bucket
+    encrypt = true
+    profile = "791678585244_darkops-lenovo.admin"
+  }
+}
---
+++ variables.tfvars
@@ -0,0 +1,6 @@
+# variables.tfvars
+
+# You can define variables here if needed
+# For example:
+# aws_region = "us-east-1"
+# bucket_name = "my-unique-bucket-name"
