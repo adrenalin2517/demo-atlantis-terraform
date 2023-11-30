4ae92d0eea6db664e6a0d3908ee1beef818de3e3
adrenalin2517 <47602588+adrenalin2517@users.noreply.github.com>
2023-11-30 18:44:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,10 +16,6 @@ resource "aws_s3_bucket" "my_bucket_3" {
   bucket = "demo-atlantis-bucket-3" # Set a unique bucket name
 }
 
-resource "aws_s3_bucket" "my_bucket_4" {
-  bucket = "demo-atlantis-bucket-4" # Set a unique bucket name
-}
-
 terraform {
   backend "s3" {
     bucket  = "terraform-atlantis-state" # Set a unique bucket name for storing Terraform state
